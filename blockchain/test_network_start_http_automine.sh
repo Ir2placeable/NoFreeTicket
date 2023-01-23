90f9a1cd83f29103ae79001494889f1e34c144c6
ir2placeable <zxcv123594@gmail.com>
2023-01-23 21:50:57 +0900

    add : start shell with automine

---
+++ blockchain/test_network_start_http_automine.sh
@@ -0,0 +1,1 @@
+geth --networkid 4298 --nodiscover --allow-insecure-unlock --maxpeers 0 --datadir test_network --http --http.addr "0.0.0.0" --http.port 8545 --http.corsdomain "*" --http.api "admin,eth,miner,personal,web3" --preload "./automine.js" console
