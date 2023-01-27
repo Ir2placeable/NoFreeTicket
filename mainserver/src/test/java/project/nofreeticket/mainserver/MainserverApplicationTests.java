6e8eff737a9ed04ce186aede4bb8d626f823d694
ir2placeable <zxcv123594@gmail.com>
2023-01-27 20:28:49 +0900

    project initializing

---
+++ mainserver/src/test/java/project/nofreeticket/mainserver/MainserverApplicationTests.java
@@ -0,0 +1,13 @@
+package project.nofreeticket.mainserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
